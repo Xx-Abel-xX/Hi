40629b32db41550ba9bac16010fc2054b2604d23
unknown <abel3647@gmail.com>
2023-10-28 08:42:49 -0400

    first commit

---
+++ streaming-client-api.js
@@ -0,0 +1,367 @@
+'use strict';
+import DID_API from './api.json' assert { type: 'json' };
+
+if (DID_API.key == '🤫') alert('Please put your api key inside ./api.json and restart..');
+
+const RTCPeerConnection = (
+  window.RTCPeerConnection ||
+  window.webkitRTCPeerConnection ||
+  window.mozRTCPeerConnection
+).bind(window);
+
+let peerConnection;
+let streamId;
+let sessionId;
+let sessionClientAnswer;
+
+let statsIntervalId;
+let videoIsPlaying;
+let lastBytesReceived;
+
+const talkVideo = document.getElementById('talk-video');
+talkVideo.setAttribute('playsinline', '');
+const peerStatusLabel = document.getElementById('peer-status-label');
+const iceStatusLabel = document.getElementById('ice-status-label');
+const iceGatheringStatusLabel = document.getElementById('ice-gathering-status-label');
+const signalingStatusLabel = document.getElementById('signaling-status-label');
+const streamingStatusLabel = document.getElementById('streaming-status-label');
+
+const connectButton = document.getElementById('connect-button');
+connectButton.onclick = async () => {
+  if (peerConnection && peerConnection.connectionState === 'connected') {
+    return;
+  }
+
+  stopAllStreams();
+  closePC();
+
+  const sessionResponse = await fetchWithRetries(`${DID_API.url}/talks/streams`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${DID_API.key}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      source_url: 'https://d-id-public-bucket.s3.amazonaws.com/or-roman.jpg',
+    }),
+  });
+
+  const { id: newStreamId, offer, ice_servers: iceServers, session_id: newSessionId } = await sessionResponse.json();
+  streamId = newStreamId;
+  sessionId = newSessionId;
+
+  try {
+    sessionClientAnswer = await createPeerConnection(offer, iceServers);
+  } catch (e) {
+    console.log('error during streaming setup', e);
+    stopAllStreams();
+    closePC();
+    return;
+  }
+
+  const sdpResponse = await fetch(`${DID_API.url}/talks/streams/${streamId}/sdp`, {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${DID_API.key}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      answer: sessionClientAnswer,
+      session_id: sessionId,
+    }),
+  });
+};
+
+
+document.querySelector('.generar').addEventListener('click',speech)
+
+      const API_KEY = "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";
+      const ELEVEN_LABS_API_KEY="bedb9645d3255a6c8611aca09e899976"
+
+  let oIa;
+  let ia;
+async function speech(){
+    const reconocer=new webkitSpeechRecognition()
+    reconocer.continuous=true;
+    reconocer.interimResults=false;
+    reconocer.lang="ES";
+    reconocer.start();
+    reconocer.onresult=(e)=>{
+
+      console.log(e.results[0][0].transcript)
+      oIa=e.results[0][0].transcript;
+      
+      handleSend(oIa)
+    }
+}
+
+  const handleSend = async (s) => {
+    await processMessageToChatGPT(s)
+  };
+  async function processMessageToChatGPT(chatMessages) { 
+    await fetch("https://api.openai.com/v1/chat/completions", 
+    {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [ 
+          {role: "system", content: 'eres un asistente malhablado'},
+          {role: "user", content: chatMessages}
+        ],
+      })
+    }).then((data) => {
+      return data.json();
+    }).then(async (data) => {
+      ia=data.choices[0].message.content
+      console.log(ia)
+      let s=data.choices[0].message.content
+      return await elevenlabs(s)
+    })
+  }
+  async function elevenlabs(s){
+    var oHttp = new XMLHttpRequest();
+    oHttp.open("POST", "https://api.elevenlabs.io/v1/text-to-speech/pNInz6obpgDQGcFmaJgB");
+    oHttp.setRequestHeader("Accept", "audio/mpeg");
+    oHttp.setRequestHeader("Content-Type", "application/json");
+    oHttp.setRequestHeader("xi-api-key", ELEVEN_LABS_API_KEY)
+    oHttp.onload = function () {
+        if (oHttp.readyState === 4) {
+            var oBlob = new Blob([this.response], { "type": "audio/mpeg" });
+            var audioURL = window.URL.createObjectURL(oBlob);
+            var audio = new Audio();
+            audio.src = audioURL;
+            console.log(audio.src)
+        }
+      }
+    var data = {
+      text: s,
+      voice_settings: { stability: 0, similarity_boost: 0 }
+    };
+
+    oHttp.responseType = "arraybuffer";
+    oHttp.send(JSON.stringify(data));
+    console.log(JSON.stringify(data))
+
+    await talkButton()
+
+  }
+
+async function talkButton() {
+  // connectionState not supported in firefox
+  if (peerConnection?.signalingState === 'stable' || peerConnection?.iceConnectionState === 'connected') {
+    const talkResponse = await fetchWithRetries(`${DID_API.url}/talks/streams/${streamId}`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${DID_API.key}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        script: {
+          type: 'audio',
+          audio_url: 'https://d-id-public-bucket.s3.us-west-2.amazonaws.com/webrtc.mp3',
+        },
+        driver_url: 'bank://lively/',
+        config: {
+          stitch: true,
+        },
+        session_id: sessionId,
+      }),
+    });
+  }
+};
+
+const destroyButton = document.getElementById('destroy-button');
+destroyButton.onclick = async () => {
+  await fetch(`${DID_API.url}/talks/streams/${streamId}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: `Basic ${DID_API.key}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ session_id: sessionId }),
+  });
+
+  stopAllStreams();
+  closePC();
+};
+
+function onIceGatheringStateChange() {
+  iceGatheringStatusLabel.innerText = peerConnection.iceGatheringState;
+  iceGatheringStatusLabel.className = 'iceGatheringState-' + peerConnection.iceGatheringState;
+}
+function onIceCandidate(event) {
+  console.log('onIceCandidate', event);
+  if (event.candidate) {
+    const { candidate, sdpMid, sdpMLineIndex } = event.candidate;
+
+    fetch(`${DID_API.url}/talks/streams/${streamId}/ice`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Basic ${DID_API.key}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        candidate,
+        sdpMid,
+        sdpMLineIndex,
+        session_id: sessionId,
+      }),
+    });
+  }
+}
+function onIceConnectionStateChange() {
+  iceStatusLabel.innerText = peerConnection.iceConnectionState;
+  iceStatusLabel.className = 'iceConnectionState-' + peerConnection.iceConnectionState;
+  if (peerConnection.iceConnectionState === 'failed' || peerConnection.iceConnectionState === 'closed') {
+    stopAllStreams();
+    closePC();
+  }
+}
+function onConnectionStateChange() {
+  // not supported in firefox
+  peerStatusLabel.innerText = peerConnection.connectionState;
+  peerStatusLabel.className = 'peerConnectionState-' + peerConnection.connectionState;
+}
+function onSignalingStateChange() {
+  signalingStatusLabel.innerText = peerConnection.signalingState;
+  signalingStatusLabel.className = 'signalingState-' + peerConnection.signalingState;
+}
+
+function onVideoStatusChange(videoIsPlaying, stream) {
+  let status;
+  if (videoIsPlaying) {
+    status = 'streaming';
+    const remoteStream = stream;
+    setVideoElement(remoteStream);
+  } else {
+    status = 'empty';
+    playIdleVideo();
+  }
+  streamingStatusLabel.innerText = status;
+  streamingStatusLabel.className = 'streamingState-' + status;
+}
+
+function onTrack(event) {
+  /**
+   * The following code is designed to provide information about wether currently there is data
+   * that's being streamed - It does so by periodically looking for changes in total stream data size
+   *
+   * This information in our case is used in order to show idle video while no talk is streaming.
+   * To create this idle video use the POST https://api.d-id.com/talks endpoint with a silent audio file or a text script with only ssml breaks 
+   * https://docs.aws.amazon.com/polly/latest/dg/supportedtags.html#break-tag
+   * for seamless results use `config.fluent: true` and provide the same configuration as the streaming video
+   */
+
+  if (!event.track) return;
+
+  statsIntervalId = setInterval(async () => {
+    const stats = await peerConnection.getStats(event.track);
+    stats.forEach((report) => {
+      if (report.type === 'inbound-rtp' && report.mediaType === 'video') {
+        const videoStatusChanged = videoIsPlaying !== report.bytesReceived > lastBytesReceived;
+
+        if (videoStatusChanged) {
+          videoIsPlaying = report.bytesReceived > lastBytesReceived;
+          onVideoStatusChange(videoIsPlaying, event.streams[0]);
+        }
+        lastBytesReceived = report.bytesReceived;
+      }
+    });
+  }, 500);
+}
+
+async function createPeerConnection(offer, iceServers) {
+  if (!peerConnection) {
+    peerConnection = new RTCPeerConnection({ iceServers });
+    peerConnection.addEventListener('icegatheringstatechange', onIceGatheringStateChange, true);
+    peerConnection.addEventListener('icecandidate', onIceCandidate, true);
+    peerConnection.addEventListener('iceconnectionstatechange', onIceConnectionStateChange, true);
+    peerConnection.addEventListener('connectionstatechange', onConnectionStateChange, true);
+    peerConnection.addEventListener('signalingstatechange', onSignalingStateChange, true);
+    peerConnection.addEventListener('track', onTrack, true);
+  }
+
+  await peerConnection.setRemoteDescription(offer);
+  console.log('set remote sdp OK');
+
+  const sessionClientAnswer = await peerConnection.createAnswer();
+  console.log('create local sdp OK');
+
+  await peerConnection.setLocalDescription(sessionClientAnswer);
+  console.log('set local sdp OK');
+
+  return sessionClientAnswer;
+}
+
+function setVideoElement(stream) {
+  if (!stream) return;
+  talkVideo.srcObject = stream;
+  talkVideo.loop = false;
+
+  // safari hotfix
+  if (talkVideo.paused) {
+    talkVideo
+      .play()
+      .then((_) => {})
+      .catch((e) => {});
+  }
+}
+
+function playIdleVideo() {
+  talkVideo.srcObject = undefined;
+  talkVideo.src = 'or_idle.mp4';
+  talkVideo.loop = true;
+}
+
+function stopAllStreams() {
+  if (talkVideo.srcObject) {
+    console.log('stopping video streams');
+    talkVideo.srcObject.getTracks().forEach((track) => track.stop());
+    talkVideo.srcObject = null;
+  }
+}
+
+function closePC(pc = peerConnection) {
+  if (!pc) return;
+  console.log('stopping peer connection');
+  pc.close();
+  pc.removeEventListener('icegatheringstatechange', onIceGatheringStateChange, true);
+  pc.removeEventListener('icecandidate', onIceCandidate, true);
+  pc.removeEventListener('iceconnectionstatechange', onIceConnectionStateChange, true);
+  pc.removeEventListener('connectionstatechange', onConnectionStateChange, true);
+  pc.removeEventListener('signalingstatechange', onSignalingStateChange, true);
+  pc.removeEventListener('track', onTrack, true);
+  clearInterval(statsIntervalId);
+  iceGatheringStatusLabel.innerText = '';
+  signalingStatusLabel.innerText = '';
+  iceStatusLabel.innerText = '';
+  peerStatusLabel.innerText = '';
+  console.log('stopped peer connection');
+  if (pc === peerConnection) {
+    peerConnection = null;
+  }
+}
+
+const maxRetryCount = 3;
+const maxDelaySec = 4;
+
+async function fetchWithRetries(url, options, retries = 1) {
+  try {
+    return await fetch(url, options);
+  } catch (err) {
+    if (retries <= maxRetryCount) {
+      const delay = Math.min(Math.pow(2, retries) / 4 + Math.random(), maxDelaySec) * 1000;
+
+      await new Promise((resolve) => setTimeout(resolve, delay));
+
+      console.log(`Request failed, retrying ${retries}/${maxRetryCount}. Error ${err}`);
+      return fetchWithRetries(url, options, retries + 1);
+    } else {
+      throw new Error(`Max retries exceeded. error: ${err}`);
+    }
+  }
+}
